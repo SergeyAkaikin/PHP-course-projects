e170313b1ac8a718c0d79587b1bf2047b6af003b
Sergey Akaikin <sergey_rm@vk.com>
2022-10-14 22:56:33 +0300

    Lab2 BisectionMethod

--- Lab-2/BisectionMethod/Main.php
+++ Lab-2/BisectionMethod/Main.php
@@ -10,8 +10,30 @@ function func(float $x): float
 $a = 0.0;
 $b = 2.0;
 $eps = 0.0001;
+$i = 0;
 
-$y = func(($b - $a) / 2);
+do {
 
-echo "Корень уравнения равен {$y}\n";
-echo "Количество итераций равно " . 1;
+    $x0 = ($a + $b) / 2.0;
+
+    if (func($x0) * func($a) < 0) $b = $x0;
+    else $a = $x0;
+
+    $i++;
+
+} while(abs(func($x0)) >= $eps);
+
+$x0 = ($a + $b) / 2.0;
+$signNum = 0; //в signNum будет храниться число верных знаков после запятой, зависит от точности eps
+if ($eps <= 1) {
+
+    $epsSt = (string)$eps;
+    $n = explode( '.', $epsSt)[1];
+    if (strpos($n, 'E')) $signNum = (float)substr($n, strpos($n, 'E') + 2);
+    else $signNum = strlen($n);
+}
+
+
+printf( "Корень уравнения равен %.{$signNum}f\n", $x0);
+echo "Количество итераций равно " . $i;
+
